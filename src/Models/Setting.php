83c60fb888f5341014db45603ed3b3513ff2e0aa
Sinevia <info@sinevia.com>
2019-01-01 02:12:23 +0000

    Create Setting.php

---
+++ src/Models/Setting.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Sinevia\Settings;
+
+class Setting extends \App\Models\BaseModel {
+
+    protected $table = 'snv_settings_setting';
+    protected $primaryKey = 'Id';
+
+    public static function tableCreate() {
+        $o = new Setting;
+        return \Schema::connection($o->connection)->create($o->table, function (\Illuminate\Database\Schema\Blueprint $table) use ($o) {
+                    $table->engine = 'InnoDB';
+                    $table->string($o->primaryKey, 40)->primary();
+                    $table->string('Key', 255);
+                    $table->text('Value');
+                    $table->datetime('CreatedAt')->nullable()->default(null);
+                    $table->datetime('UpdatedAt')->nullable()->default(null);
+                    $table->datetime('DeletedAt')->nullable()->default(null);
+                });
+    }
+
+    public static function tableDelete() {
+        $o = new Setting;
+        return \Schema::connection($o->connection)->drop($o->table);
+    }
+
+}
